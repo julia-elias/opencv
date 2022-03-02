ffba7b3f7da39c9409e22420c9f42f39c5587f0b
Julia Elias <67982703+julia-elias@users.noreply.github.com>
2022-03-01 21:31:34 -0600

    Add files via upload

---
+++ ReadAVI.cpp
@@ -0,0 +1,28 @@
+#include "ReadAVI.h"
+#include "ColorFilter.h"
+
+ReadAVI::ReadAVI(std::string filename, ColorFilter &cf) :
+    _cf(cf), _cap(filename) {}
+
+void ReadAVI::processFile() {
+    cv::namedWindow("colors");
+    getNextFrame();
+    while(!nextFrameEmpty()) {
+        _cf.processImage(_frame);
+        cv::waitKey(200);        
+        getNextFrame();
+        
+    }
+}
+
+void ReadAVI::getNextFrame() {
+    _cap >> _frame;
+}
+
+bool ReadAVI::nextFrameEmpty() {
+    if (_frame.empty()) {
+        return true;
+    } else {
+        return false;
+    }
+}
